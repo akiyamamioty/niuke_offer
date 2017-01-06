94d4269c40821b33c0e238fafac552dc1ff05908
akiyamamioty <672795574@qq.com>
2017-01-06 22:50:31 +0800

    hassubtree

---
+++ HasSubTree.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def issubtree(self, pRoot1, pRoot2):
+        if not pRoot2:
+            return True
+        if not pRoot1:
+            return False
+        if pRoot1.val != pRoot2.val:
+            return False
+        return self.issubtree(pRoot1.left, pRoot2.left) and self.issubtree(pRoot1.right, pRoot2.right)
+    def HasSubtree(self, pRoot1, pRoot2):
+        result = False
+        if pRoot1 and pRoot2:
+            if pRoot1.val == pRoot2.val:
+                result = self.issubtree(pRoot1, pRoot2)
+            if not result:
+                result = self.HasSubtree(pRoot1.left, pRoot2)
+            if not result:
+                result = self.HasSubtree(pRoot1.right, pRoot2)
+        return result
